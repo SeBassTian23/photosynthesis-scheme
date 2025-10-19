fb8036f5f4b3f223ac896ea1e17e27ca6c1b1bb9
Sebastian <sebastian.kuhlgert@gmail.com>
2025-10-19 10:22:50 +0200

    fix adding class names

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ export default class Photosynthesis {
         this.#draw = SVG().addTo(this.target).size( this.width, this.height );
         // Add class
         if(this.className)
-          this.#draw.addClass('calendar-heatmap');
+          this.#draw.addClass(this.class);
         // Add css styles
         if(this.style)
           this.#draw.css(style);
